5a40254ca5e732886f033a3013306dc27358e9b4
Murod <murod1bakhramov@gmail.com>
2025-07-31 11:16:47 +0500

    Show selected tariff name, specs and price in popup when ordering on colocation page

--- resources/views/components/order-popup.blade.php
+++ resources/views/components/order-popup.blade.php
@@ -1,5 +1,5 @@
 <div class="order-popup fixed left-0 top-0 flex hidden h-screen w-screen items-center justify-center bg-black/50 transition-all duration-300">
-    <div class="w-140 relative rounded-lg bg-white p-10 text-secondary">
+    <div class="w-140 relative rounded-lg bg-white p-5 md:p-10 text-secondary">
         <h1 class="m-0 mb-5 font-medium">Заказ</h1>
         <div class="mb-5 text-sm leading-relaxed text-secondary" id="order-details">
             <div class="text-lg font-semibold text-button-bg" id="popup-tariff-name">...</div>
@@ -9,16 +9,16 @@
         <form class="flex justify-between gap-5" action="">
             <input class="w-full rounded-lg border border-secondary/50 px-3 py-2 shadow-sm focus:border-button-bg focus:outline-none focus:ring-1 focus:ring-button-bg" name="your-name" type="text"
                 placeholder="Ваше имя">
-            <input class="w-full rounded-lg border border-secondary/50 px-3 py-2 shadow-sm focus:border-button-bg focus:outline-none focus:ring-1 focus:ring-button-bg" name="phone" type="phone"
+            <input class="w-full rounded-lg border border-secondary/50 px-2 py-1 md:px-3 md:py-2 shadow-sm focus:border-button-bg focus:outline-none focus:ring-1 focus:ring-button-bg" name="phone" type="phone"
                 placeholder="Номер телефона">
         </form>
         <div class="">
-            <button class="w-full cursor-pointer rounded-md border-0 bg-button-bg px-2.5 py-2 font-futura text-xs/3 uppercase text-white transition hover:bg-[#5b5fe6] md:px-4 md:py-3 md:text-sm"
+            <button class="w-full cursor-pointer rounded-md border-0 bg-button-bg px-3 py-3 font-futura text-xs/3 uppercase text-white transition hover:bg-[#5b5fe6] md:px-4 md:py-3 md:text-sm"
                 type="submit">Заказать</button>
         </div>
         <div class="mt-5 flex items-center gap-2">
             <input id="consent" name="consent" type="checkbox" required />
-            <label class="text-base" for="consent">
+            <label class="text-base leading-none" for="consent">
                 Даю согласие на обработку и хранение моих персональных данных
             </label>
         </div>
